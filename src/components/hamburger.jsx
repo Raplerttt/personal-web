56716adb830581c1067f9413be163977e7e0dadd
mac <mac@macs-MacBook-Pro.local>
2024-11-19 15:20:33 +0700

    test

--- src/components/hamburger.jsx
+++ src/components/hamburger.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaHome, FaUserAlt, FaBriefcase, FaCertificate, FaBars } from 'react-icons/fa'; // Mengimpor ikon dari React Icons
+import { FaHome, FaUserAlt, FaBriefcase, FaCertificate, FaBars, FaStudiovinari } from 'react-icons/fa'; // Mengimpor ikon dari React Icons
 
 const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State untuk menampilkan sidebar
@@ -58,6 +58,14 @@ const Sidebar = () => {
           >
             <FaCertificate className="h-6 w-6" />
             <span className="absolute left-6 px-4 py-2 opacity-0 group-hover:opacity-100 transition-opacity rounded-lg duration-300 ease-in-out">Certificate</span>
+          </button>
+
+          <button
+            className="group relative flex items-center space-x-2 hover:bg-red-700 hover:text-white p-2 rounded-lg w-full"
+            onClick={() => window.location.href = '/sertificate'} // Menavigasi ke halaman Sertifikat
+          >
+            <FaStudiovinari className="h-6 w-6" />
+            <span className="absolute left-6 px-4 py-2">Certificate</span>
           </button>
         </div>
       </div>
